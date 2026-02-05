2dc23a418b3bed9fc5fa7b6e5d28cd825c7b2a1d
tom <tom.g.woodward@gmail.com>
2026-02-05 16:29:15 -0500

    fix example

--- packages/cutie-example/src/example-items/inline-choice-multi.ts
+++ packages/cutie-example/src/example-items/inline-choice-multi.ts
@@ -10,19 +10,19 @@ https://purl.imsglobal.org/spec/qti/v3p0/schema/xsd/imsqti_asiv3p0p1_v1p0.xsd"
 identifier="inline-choice-multi" title="Inline Choice - Multiple in Paragraph"
 adaptive="false" time-dependent="false" xml:lang="en">
 
-  <qti-response-declaration identifier="RESPONSE1" cardinality="single" base-type="identifier">
+  <qti-response-declaration identifier="RESPONSE" cardinality="single" base-type="identifier">
     <qti-correct-response>
       <qti-value>photosynthesis</qti-value>
     </qti-correct-response>
   </qti-response-declaration>
 
-  <qti-response-declaration identifier="RESPONSE2" cardinality="single" base-type="identifier">
+  <qti-response-declaration identifier="RESPONSE_2" cardinality="single" base-type="identifier">
     <qti-correct-response>
       <qti-value>sunlight</qti-value>
     </qti-correct-response>
   </qti-response-declaration>
 
-  <qti-response-declaration identifier="RESPONSE3" cardinality="single" base-type="identifier">
+  <qti-response-declaration identifier="RESPONSE_3" cardinality="single" base-type="identifier">
     <qti-correct-response>
       <qti-value>oxygen</qti-value>
     </qti-correct-response>
@@ -44,19 +44,19 @@ adaptive="false" time-dependent="false" xml:lang="en">
     <p>Select the correct words to complete the paragraph about plant biology.</p>
     <p>
       Plants produce their own food through a process called
-      <qti-inline-choice-interaction response-identifier="RESPONSE1" shuffle="true">
+      <qti-inline-choice-interaction response-identifier="RESPONSE" shuffle="true">
         <qti-inline-choice identifier="photosynthesis">photosynthesis</qti-inline-choice>
         <qti-inline-choice identifier="respiration">respiration</qti-inline-choice>
         <qti-inline-choice identifier="fermentation">fermentation</qti-inline-choice>
       </qti-inline-choice-interaction>.
       This process converts carbon dioxide and water into glucose using energy from
-      <qti-inline-choice-interaction response-identifier="RESPONSE2" shuffle="true">
+      <qti-inline-choice-interaction response-identifier="RESPONSE_2" shuffle="true">
         <qti-inline-choice identifier="sunlight">sunlight</qti-inline-choice>
         <qti-inline-choice identifier="soil">soil</qti-inline-choice>
         <qti-inline-choice identifier="wind">wind</qti-inline-choice>
       </qti-inline-choice-interaction>.
       As a byproduct, plants release
-      <qti-inline-choice-interaction response-identifier="RESPONSE3" shuffle="true">
+      <qti-inline-choice-interaction response-identifier="RESPONSE_3" shuffle="true">
         <qti-inline-choice identifier="oxygen">oxygen</qti-inline-choice>
         <qti-inline-choice identifier="nitrogen">nitrogen</qti-inline-choice>
         <qti-inline-choice identifier="methane">methane</qti-inline-choice>
@@ -77,16 +77,16 @@ adaptive="false" time-dependent="false" xml:lang="en">
     <qti-set-outcome-value identifier="SCORE">
       <qti-sum>
         <qti-match>
-          <qti-variable identifier="RESPONSE1"/>
-          <qti-correct identifier="RESPONSE1"/>
+          <qti-variable identifier="RESPONSE"/>
+          <qti-correct identifier="RESPONSE"/>
         </qti-match>
         <qti-match>
-          <qti-variable identifier="RESPONSE2"/>
-          <qti-correct identifier="RESPONSE2"/>
+          <qti-variable identifier="RESPONSE_2"/>
+          <qti-correct identifier="RESPONSE_2"/>
         </qti-match>
         <qti-match>
-          <qti-variable identifier="RESPONSE3"/>
-          <qti-correct identifier="RESPONSE3"/>
+          <qti-variable identifier="RESPONSE_3"/>
+          <qti-correct identifier="RESPONSE_3"/>
         </qti-match>
       </qti-sum>
     </qti-set-outcome-value>
